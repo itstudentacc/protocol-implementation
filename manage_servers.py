7ea1caac8c3891f3228257e952946acac2484af9
GregoriusNuraga <gregorius@backup-lenovo>
2024-08-28 11:56:21 +0930

    Added skeleton files and instructions for neighbourhood client
    Not final, subject to change.

---
+++ manage_servers.py
@@ -0,0 +1,42 @@
+### manage_servers.py
+
+### INSTRUCTIONS
+
+## 1) list_neighborhood_servers()
+    #   Purpose: This function lists all the servers in the neighborhood.
+    #   Input: None
+    #   Output: returns the list of servers in the neighborhood.
+    #   Implementation criteria:
+    #   - Server can choose what to do with the list of servers.
+    #   - Server can choose how to handle the list of servers.
+    #   - Display the list of servers.
+
+def list_neighborhood_servers():
+
+## 2) synchronize_client_list()
+    # Purpose :  function ensure all servers has the same current info about the client
+    # Input :
+    # Output: 
+    # Implementation criteria:
+    #
+
+def synchronize_client_list():
+
+## 3) update_client_info(client)
+    # Purpose :  function to update the client information
+    # Input : client
+    # Output: 
+    # Implementation criteria:
+    #
+
+def update_client_info(client):
+
+
+## 4) add_server_to_neighborhood(server)
+    # Purpose :  function to add a server to the neighborhood
+    # Input : server
+    # Output: 
+    # Implementation criteria:
+    #
+
+def add_server_to_neighborhood(server):
---
+++ server_to_server.py
@@ -0,0 +1,28 @@
+### server_to_server.py
+
+## INSTRUCTIONS
+
+## 1) Server to Server Communication
+    #   Purpose: This function sends a message from one server to another server and returns the response.  
+    #   Input: takes in the destination server (dest) and the message to be sent.
+    #   Output: returns the response from the server.
+    #   Implementation criteria:
+    #   - Server can choose which server to send the message to.
+    #   - Server can choose what message to send.
+    #   - Server can choose what to do with the response.
+    #   - Server can choose how to handle the response.
+
+
+def server_to_server(dest,message):
+
+ 
+## 2) broadcast_message_to_all(message)
+    #   Purpose: This function sends a message to all servers in the network.
+    #   Input: takes in the message to be sent.
+    #   Output: announce the message to all servers in the neighborhood.
+    #   Implementation criteria:
+    #   - Server can choose what message to send.
+    #   - Server can choose what to do with the response.
+    #   - Server can choose how to handle the response.
+
+def broadcast_message_to_all(message):
