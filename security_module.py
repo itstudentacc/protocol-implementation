7412c96c3339757da01c9f02b59aeb9c26fd28df
GregoriusNuraga <gregorius@backup-lenovo>
2024-09-07 23:01:49 +0930

    Added rsa key generator functions (not yet tested)

--- security_module.py
+++ security_module.py
@@ -1,5 +1,19 @@
 # security_module.py
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import pkcs1_15
+
 from utility import encode_data, decode_data
+
+
+# Generate the RSA key using the imported library
+def generate_RSA_key():
+    key = RSA.generate(2048)
+    publicKey = key.publickey().export_key().decode('utf-8')
+    privateKey = key.export_key().decode('utf-8')
+    return privateKey,publicKey
+
+
 
 
 
